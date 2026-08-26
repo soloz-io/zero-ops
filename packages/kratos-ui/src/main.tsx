e4eb5d3c7f78b8a0cfaef5c8a0a1b14eb905e858
arun4infra <arun4infra@gmail.com>
2026-08-26 08:47:19 +0530

    fix(kratos-ui): wrap app in OryProvider

--- packages/kratos-ui/src/main.tsx
+++ packages/kratos-ui/src/main.tsx
@@ -2,11 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { RouterProvider } from 'react-router-dom';
 import { router } from './app/router';
+import { OryProvider } from './contexts/ory-context';
 import '@ory/elements-react/theme/styles.css';
 import './app/globals.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <OryProvider>
+      <RouterProvider router={router} />
+    </OryProvider>
   </React.StrictMode>
 );
